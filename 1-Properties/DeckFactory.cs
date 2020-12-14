b10605150701a063f245ae54a7ff375823cdc31a
Matteo Vanni <matteo.vanni2@studio.unibo.it>
2020-12-14 12:31:06 +0100

    GetDeck modificato con metodo giusto

--- 1-Properties/DeckFactory.cs
+++ 1-Properties/DeckFactory.cs
@@ -31,22 +31,17 @@ namespace Properties
         }
 
         /// TODO improve
-        public ISet<Card> GetDeck()
+        public ISet<Card> GetDeck
         {
-            if (this.names == null || this.seeds == null)
-            {
-                throw new InvalidOperationException();
-            }
-
-            return new HashSet<Card>(Enumerable
-                .Range(0, this.names.Length)
-                .SelectMany(i => Enumerable
-                    .Repeat(i, this.seeds.Length)
-                    .Zip(
-                        Enumerable.Range(0, this.seeds.Length),
-                        (n, s) => Tuple.Create(this.names[n], this.seeds[s], n)))
-                .Select(tuple => new Card(tuple))
-                .ToList());
+            get { return new HashSet<Card>(Enumerable
+                  .Range(0, this.names.Length)
+                  .SelectMany(i => Enumerable
+                      .Repeat(i, this.seeds.Length)
+                      .Zip(
+                          Enumerable.Range(0, this.seeds.Length),
+                          (n, s) => Tuple.Create(this.names[n], this.seeds[s], n)))
+                  .Select(tuple => new Card(tuple))
+                  .ToList()); }
         }
     }
 }
